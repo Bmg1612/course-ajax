34af0974fb20455f228e016a29768ceca0135ea5
Bruno Gurgel <bmg1612@gmail.com>
2020-11-18 11:33:32 -0300

    docs: Initial commit

--- lesson-1-async-w-xhr/app.js
+++ lesson-1-async-w-xhr/app.js
@@ -1,12 +1,69 @@
-(function () {
-    const form = document.querySelector('#search-form');
-    const searchField = document.querySelector('#search-keyword');
-    let searchedForText;
-    const responseContainer = document.querySelector('#response-container');
+const form = document.querySelector('#search-form');
+const searchField = document.querySelector('#search-keyword');
+let searchedForText;
+const responseContainer = document.querySelector('#response-container');
 
+(function () {
     form.addEventListener('submit', function (e) {
         e.preventDefault();
         responseContainer.innerHTML = '';
         searchedForText = searchField.value;
+
+        const imgRequest = new XMLHttpRequest();
+        imgRequest.onload = addImage;
+        imgRequest.onerror = function (err) {
+            requestError(err, 'image');
+    };
+    imgRequest.open('GET', `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`);
+    imgRequest.setRequestHeader('Authorization', 'Client-ID v5FFgYLlEp9xyUOiZVrjtJGfwov_MXDs3WcWqCV52Fo');
+    imgRequest.send();
+
+    const articleRequest = new XMLHttpRequest;
+    imgRequest.onload = addArticles;
+        imgRequest.onerror = function (err) {
+            requestError(err, 'articles');
+    };
+    articleRequest.open('GET', `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=TPLAvw7mn3TFohV8AgfAj4SoxWhuPkQz`);
+    articleRequest.send();
+    
     });
 })();
+
+
+function addImage(){
+    let htmlContent = "";
+    const data = JSON.parse(this.responseText);
+
+    if(data && data.results && data.results[0]) {
+    const firstImage = data.results[0];
+
+    htmlContent = `<figure>
+    <img src="${firstImage.urls.regular}" alt="${searchedForText}">
+    <figcaption>${searchedForText} by ${firstImage.user.name}</figcaption>
+    </figure>`;
+} else {
+    htmlContent = '<div class="error-no-image">No images available</div>';
+}
+
+    responseContainer.insertAdjacentHTML('afterbegin', htmlContent);
+};
+
+function addArticles () {
+    let htmlContent = "";
+    const data = JSON.parse(this.responseText);
+
+    if(data.response && data.response.docs && data.response.docs.length>1) {
+    const firstImage = data.results[0];
+
+    htmlContent = '<ul>' + data.response.docs.map(article => `<li class="article">
+    <h2><a href="${article.web_url}">${article.headline.main}</a></h2>
+    <p>${article.snippet}</p>
+    </li>`
+    ).join('') + '</ul>'
+} else {
+    htmlContent = '<div class="error-no-articles">No articles available</div>';
+}
+
+    responseContainer.insertAdjacentHTML('beforeend', htmlContent);
+};
+
